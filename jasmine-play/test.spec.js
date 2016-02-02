e433f57cc290cf376b96e15e2731bf5cfdbf6a81
panox <panospalos@gmail.com>
2016-02-02 21:31:55 +0200

    wrote test to check method for reversed name

--- jasmine-play/test.spec.js
+++ jasmine-play/test.spec.js
@@ -1,5 +1,11 @@
 var Word = require('./test.js');
 
 describe('Word', function() {
-  var word = new Word();
+  var word = new Word("panos");
+
+  it('reverse the word', function() {
+    word.reverse();
+    expect(word.name).toEqual('sonap');
+  });
+
 });
